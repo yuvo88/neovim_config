ac2cb876dbe53792a64568c2342f26270b4c03dc
Yuval Schnitzer <yuvals@tamnoon.io>
2025-09-21 01:33:14 -0400

    formatting and watching for file changes

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,78 +1,81 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			{
-				"folke/lazydev.nvim",
-				ft = "lua", -- only load on lua files
-				opts = {
-					library = {
-						-- See the configuration section for more details
-						-- Load luvit types when the `vim.uv` word is found
-						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-					},
-				},
-			},
-			"hrsh7th/cmp-nvim-lsp",
-		},
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local on_attach = function(client, bufnr)
-				local bufopts = { noremap = true, silent = true, buffer = bufnr }
-				local map = vim.keymap.set
-				map({ "n", "i" }, "<C-k>", vim.lsp.buf.signature_help, bufopts)
-				map("n", "K", vim.lsp.buf.hover, bufopts)
-				map("n", "gD", vim.lsp.buf.declaration, bufopts)
-				map("n", "gd", function()
-					vim.lsp.buf.definition()
-				end, bufopts)
-				map("n", "<leader>e", vim.diagnostic.open_float, bufopts)
-				map("n", "<leader>.", vim.lsp.buf.code_action, bufopts)
-				map("n", "<leader>r", vim.lsp.buf.rename, bufopts)
-				client.server_capabilities.semanticTokensProvider = nil
-			end
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            {
+                "folke/lazydev.nvim",
+                ft = "lua", -- only load on lua files
+                opts = {
+                    library = {
+                        -- See the configuration section for more details
+                        -- Load luvit types when the `vim.uv` word is found
+                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                    },
+                },
+            },
+            "hrsh7th/cmp-nvim-lsp",
+        },
+        config = function()
+            local lspconfig = require("lspconfig")
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            capabilities.workspace = capabilities.workspace or {}
+            capabilities.workspace.didChangeWatchedFiles = { dynamicRegistration = true }
+            local on_attach = function(client, bufnr)
+                local bufopts = { noremap = true, silent = true, buffer = bufnr }
+                local map = vim.keymap.set
+                map({ "n", "i" }, "<C-k>", vim.lsp.buf.signature_help, bufopts)
+                map("n", "K", vim.lsp.buf.hover, bufopts)
+                map("n", "gD", vim.lsp.buf.declaration, bufopts)
+                map("n", "gd", function()
+                    vim.lsp.buf.definition()
+                end, bufopts)
+                map("n", "<leader>e", vim.diagnostic.open_float, bufopts)
+                map("n", "<leader>.", vim.lsp.buf.code_action, bufopts)
+                map("n", "<leader>r", vim.lsp.buf.rename, bufopts)
+                map("n", "<leader>f", vim.lsp.buf.format, bufopts)
+                client.server_capabilities.semanticTokensProvider = nil
+            end
 
-			local servers = { "lua_ls", "bashls", "clangd" }
+            local servers = { "lua_ls", "bashls", "clangd" }
 
-			for _, server in ipairs(servers) do
-				lspconfig[server].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end
-			lspconfig["ruff"].setup({
-				on_attach = function(client)
-					client.server_capabilities.hoverProvider = false
-					client.server_capabilities.codeActionProvider = false
-				end,
-			})
-			lspconfig["basedpyright"].setup({
-				on_attach = on_attach,
-				capabilities = capabilities,
-				settings = {
-					basedpyright = {
-						analysis = {
-							autoImportCompletions = true,
-							useLibraryCodeForTypes = true,
-							diagnosticMode = "workspace",
-							autoSearchPath = true,
-							inlayHints = {
-								callArgumentNames = true,
-							},
-							typeCheckingMode = "basic",
-							ignore = { "/home/john/Tamnoon-Backend/tamnoon/*" },
-						},
-						python = {
-							-- venvPath = "/home/john/general_venv",
-							-- venv = "general_venv",
-							analysis = {
-								autoImportCompletions = true,
-							},
-						},
-					},
-				},
-			})
-		end,
-	},
+            for _, server in ipairs(servers) do
+                lspconfig[server].setup({
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                })
+            end
+            lspconfig["ruff"].setup({
+                on_attach = function(client)
+                    client.server_capabilities.hoverProvider = false
+                    client.server_capabilities.codeActionProvider = false
+                end,
+            })
+            lspconfig["basedpyright"].setup({
+                on_attach = on_attach,
+                capabilities = capabilities,
+                settings = {
+                    basedpyright = {
+                        analysis = {
+                            autoImportCompletions = true,
+                            useLibraryCodeForTypes = true,
+                            diagnosticMode = "workspace",
+                            autoSearchPath = true,
+                            inlayHints = {
+                                callArgumentNames = true,
+                            },
+                            typeCheckingMode = "basic",
+                            ignore = { "/home/john/Tamnoon-Backend/tamnoon/*" },
+                        },
+                        python = {
+                            -- venvPath = "/home/john/general_venv",
+                            -- venv = "general_venv",
+                            analysis = {
+                                autoImportCompletions = true,
+                            },
+                        },
+                    },
+                },
+            })
+        end,
+    },
 }
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,12 +1,15 @@
 return {
-  "stevearc/oil.nvim",
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  lazy = false,
-  config = function()
-    require("oil").setup({
-      columns = { "icon" },
-    })
-    vim.keymap.set("n", "-", require("oil").open)
-  end,
+    "stevearc/oil.nvim",
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    lazy = false,
+    config = function()
+        require("oil").setup({
+            columns = { "icon" },
+            watch_for_changes = true,
+            lsp_file_methods = {
+                autosave_changes = true
+            }
+        })
+        vim.keymap.set("n", "-", require("oil").open)
+    end,
 }
-
