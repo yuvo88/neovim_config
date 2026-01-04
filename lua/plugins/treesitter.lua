6c62f4beb35ce13ab669224bd3daac26fb0414e0
Yuval Schnitzer <yuvo88@gmail.com>
2026-01-03 19:34:01 -0500

    select and jump to functions

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,43 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "lua", "c" },
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = true,
-			},
-		})
-	end,
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter-textobjects"
+    },
+    config = function()
+        require("nvim-treesitter.configs").setup {
+            ensure_installed = { "lua", "c", "rust", "python", "bash", "markdown" },
+            auto_install = true,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = true,
+            },
+            textobjects = {
+                move = {
+                    enable = true,
+                    set_jumps = true, -- adds to jumplist
+                    goto_next_start = {
+                        ["<C-l>"] = "@function.outer",
+                    },
+                    goto_next_end = {
+                        ["<C-x>"] = "@function.outer",
+                    },
+                    goto_previous_start = {
+                        ["<C-h>"] = "@function.outer",
+                    },
+                    goto_previous_end = {
+                        ["<C-a>"] = "@function.outer",
+                    },
+                },
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                    }
+                },
+            },
+        }
+    end,
 }
